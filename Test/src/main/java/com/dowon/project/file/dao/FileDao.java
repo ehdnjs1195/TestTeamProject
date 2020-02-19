ae1fff8ff68e214513c92c017b890fa06a61fe89
kimtaewoo <wooziekim@gmail.com>
2020-02-19 17:12:42 +0900

    File기능 구현

---
+++ Test/src/main/java/com/dowon/project/file/dao/FileDao.java
@@ -0,0 +1,20 @@
+package com.dowon.project.file.dao;
+
+import java.util.List;
+
+import com.dowon.project.file.dto.FileDto;
+
+public interface FileDao {
+	public int getCount(FileDto dto);
+	public List<FileDto> getList(FileDto dto);
+	public void insert(FileDto dto);
+	public FileDto getData(int num);
+	public void addDownCount(int num);
+	public void delete(int num);
+}
+
+
+
+
+
+
---
+++ Test/src/main/java/com/dowon/project/file/service/FileService.java
@@ -0,0 +1,21 @@
+package com.dowon.project.file.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dowon.project.file.dto.FileDto;
+
+public interface FileService {
+	public void list(HttpServletRequest request);
+	public void saveFile(HttpServletRequest request, FileDto dto);
+	public void getFileData(ModelAndView mView, int num);
+	public void addDownCount(int num);
+	public void removeFile(HttpServletRequest request);
+}
+
+
+
+
+
+
